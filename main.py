e3381750c35bd0c0899673d18d90ee17a586fddc
shubham-mhaske <shubhzmhaske@gmail.com>
2020-03-31 14:20:49 +0530

    Added find_education module

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import  read_file
 import mobile_email
 import name
 import  find_skills
+import find_education
 
 
-nlp = spacy.load('en_core_web_md')
+nlp = spacy.load('en_core_web_sm'
+                 '+'
+                 ' gsw 0 ')
 matcher = Matcher(nlp.vocab)
 
-text = read_file.docs_to_text('/home/shubham/projects/cvparser/resumes/docs/2.doc')
+text = read_file.docs_to_text('/home/shubham/projects/cvparser/resumes/pdf/1.pdf')
 name = name.extract_name(text,nlp,matcher)
 mobileNo,email = mobile_email.get_mobile_email(text)
 skills_list = find_skills.get_skills(text)
+education = find_education.extract_education(text)
 
 
-print(name,mobileNo,email,skills_list)
+print(name,mobileNo,email,skills_list,'\n', education)
